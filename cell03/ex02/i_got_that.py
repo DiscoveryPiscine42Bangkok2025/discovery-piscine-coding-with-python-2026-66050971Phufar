6ae4ae51ec63478d649c8b8afbabc9355405bd6b
66050971Phufar <66050971@kmitl.ac.th>
2026-02-09 17:35:15 +0700

    cell03

---
+++ cell03/ex02/i_got_that.py
@@ -0,0 +1,7 @@
+#!/usr/bin/env python3
+
+init_string = input("What you gotta say? : ")
+
+while(True):
+    loop_string = input("I got that! Anything else? : ")
+    if(loop_string == "STOP"): break
