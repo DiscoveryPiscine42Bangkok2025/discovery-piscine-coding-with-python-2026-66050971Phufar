d6c15bc0c98d8384e4701a890fa831ec282ea86a
66050971Phufar <66050971@kmitl.ac.th>
2026-02-10 18:55:21 +0700

    Delete cell03/ex02/i_got_that.py

--- cell03/ex02/i_got_that.py
+++
@@ -1,7 +0,0 @@
-#!/usr/bin/env python3
-
-init_string = input("What you gotta say? : ")
-
-while(True):
-    loop_string = input("I got that! Anything else? : ")
-    if(loop_string == "STOP"): break
